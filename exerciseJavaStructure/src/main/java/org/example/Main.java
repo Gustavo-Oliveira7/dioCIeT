206e097fb4879f8b6173cd688f136c53a686fe56
Gustavo Oliveira <gustavo.h.oliveira7531@gmail.com>
2025-11-24 21:04:45 -0300

    javaStructur ex1 - done

---
+++ exerciseJavaStructure/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+//Escreva um código onde o usuário entra com um número e seja gerada a tabuada de 1 até 10 desse número;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        Integer number = 0;
+        System.out.println("Write your number:");
+        number = scanner.nextInt();
+
+        for (var i = 1; i <= 10; i++){
+            System.out.printf("%s x %s = %s \n", number, i, (number* i ));
+        }
+
+    }
+}
