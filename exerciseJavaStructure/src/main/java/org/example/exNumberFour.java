cbb34a57e17e1c9a1ae966df931bb5d56a9fbe2e
Gustavo Oliveira <gustavo.h.oliveira7531@gmail.com>
2025-12-03 22:13:08 -0300

    javaScructure ex4 - done

---
+++ exerciseJavaStructure/src/main/java/org/example/exNumberFour.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//Escreva um código onde o usuário informa um número inicial, posteriormente irá informar outros N números,
+//a execução do código irá continuar até que o número informado dividido pelo primeiro número
+//tenha resto diferente de 0 na divisão, números menores que o primeiro número devem ser ignorados
+
+
+import java.util.Scanner;
+
+public class exNumberFour {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        Integer firstNumber;
+        System.out.println("Write your first number:");
+        firstNumber = scanner.nextInt();
+        Integer numbers;
+        do {
+            System.out.println("Write another number:");
+            numbers = scanner.nextInt();
+        } while ((numbers % firstNumber == 0) || (numbers < firstNumber));
+    }
+}
+
